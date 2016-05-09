580b4cc8b1807dd2a1b72a78c1f48b0cd8a35a25
Matt Restorff <inperpetuum@gmail.com>
2016-05-09 09:50:00 -0400

    Footer mobile styles

--- wp-content/themes/Q35/footer.php
+++ wp-content/themes/Q35/footer.php
@@ -15,20 +15,20 @@
 
     <footer id="colophon" class="site-footer" role="contentinfo">
         <div class="site-info">
-                <div class="row align-justify align-middle">
+                <div class="row text-center medium-text-left large-text-left align-middle">
 
-                    <div class="column small-2">
+                    <div class="column small-12 medium-7 large-8">
                         <img src="<?php bloginfo('template_directory'); ?>/img/q-footer-logo.svg" />
                     </div>
 
-                    <div class="column small-3">
+                    <div class="column small-12 medium-5 large-4">
                         <ul class="social-icons">
-                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                            <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                            <li><a href="#"><i class="fa fa-youtube-play"></i></a></li>
-                            <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-                            <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                            <li><a href="https://www.facebook.com/QQview" target="_blank"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="https://plus.google.com/109152261638729886652" target="_blank"><i class="fa fa-google-plus"></i></a></li>
+                            <li><a href="https://twitter.com/qltd" target="_blank"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="http://www.flickr.com/photos/28113117@N04/" target="_blank"><i class="fa fa-flickr"></i></a></li>
+                            <li><a href="http://instagram.com/qltd" target="_blank"><i class="fa fa-instagram"></i></a></li>
+                            <li><a href="https://www.linkedin.com/groups/3001215/profile" target="_blank"><i class="fa fa-linkedin"></i></a></li>
                         </ul>
                         <p>&copy; <?php echo date('Y'); ?> QLTD. All rights reserved.</p>
                     </div>
